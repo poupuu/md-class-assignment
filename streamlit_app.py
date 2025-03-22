dbb43677f4653d7bc8fa2d8d7d012dbd7f8e6384
poupuu <williamanwar2@gmail.com>
2025-03-23 05:26:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,7 @@ def main():
     st.write('Data input by user')
     temp_df
 
-    temp_df = feature_encode(temp_df, label_encoders)
+    temp_df = label_encoder(temp_df, label_encoders)
     
     # #numerical inputs
     # numerical_cols = {
